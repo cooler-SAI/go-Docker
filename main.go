e6a9dc90581e5cf18ede73f7123b6edef2d4f184
cooler-SAI <cooler19860212@yahoo.com>
2024-10-25 21:48:09 +0300

    set up local server run in port

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/http"
 	"os"
 	"time"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello, you've hit %s\n", r.URL.Path)
+	if err != nil {
+		return
+	}
+}
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
@@ -15,7 +23,14 @@ func main() {
 	log.Info().Msg("Test One two")
 	log.Error().Msg("Docker Uploaded")
 
-	fmt.Println()
+	http.HandleFunc("/", handler)
+
+	go func() {
+		log.Info().Msg("Starting server on :8080")
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal().Err(err).Msg("Failed to start server")
+		}
+	}()
 
 	for {
 		log.Info().Msg("Service is running...")
