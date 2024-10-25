be52e92454d62d08e9020f694ccb593c19d77cbc
cooler-SAI <cooler19860212@yahoo.com>
2024-10-25 23:49:21 +0300

    fix up logs in main

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Info().Msg("Service RUN good!")
-	log.Error().Msg("UPDATE Last one")
+	log.Info().Msg("UPDATE Last one")
 	log.Info().Msg("Test One two")
-	log.Error().Msg("Docker Uploaded")
+	log.Info().Msg("Docker Uploaded")
 
 	http.HandleFunc("/", handler)
 
