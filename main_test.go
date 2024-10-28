89daa8650eba5d52bc5db7dc0dbbbc5e09899051
cooler-SAI <cooler19860212@yahoo.com>
2024-10-28 23:50:41 +0300

    added unit tests

---
+++ main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8080/testpath", nil)
+	w := httptest.NewRecorder()
+
+	handler(w, req)
+
+	if status := w.Result().StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Hello, you've hit /testpath\n" // Исправлено ожидаемое значение
+	if body := w.Body.String(); body != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, expected)
+	}
+}
+
+func TestMainFunction(t *testing.T) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	go func() {
+		main()
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	resp, err := http.Get("http://localhost:8080/")
+	if err != nil {
+		t.Fatalf("Could not start server: %v", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code: got %v, want %v", resp.StatusCode, http.StatusOK)
+	}
+}
